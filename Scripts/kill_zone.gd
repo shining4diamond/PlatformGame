ef60f2c727cb43e85b6144ac206a28837e4a0ddd
shining4diamond <gennaro.frezzetti1999@gmail.com>
2025-11-28 17:23:20 +0100

    Added jump and hurt sounds

--- Scripts/kill_zone.gd
+++ Scripts/kill_zone.gd
@@ -5,6 +5,7 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	print("You died!")
+	$AnimationPlayer.play("HurtSound")
 	Engine.time_scale = 0.5
 	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -9,11 +9,14 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+		
+		
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		$AnimationPlayer.play("JumpSound")
 
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
@@ -32,6 +35,7 @@ func _physics_process(delta: float) -> void:
 			$AnimatedSprite2D.play("run")
 	else:
 		$AnimatedSprite2D.play("jump")
+		
 	
 	if direction:
 		velocity.x = direction * SPEED
