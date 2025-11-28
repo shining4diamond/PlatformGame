944a0a9dc5378b82ec72932294af898187394c03
shining4diamond <gennaro.frezzetti1999@gmail.com>
2025-11-28 21:03:12 +0100

    v2
    Added new level, more enemies, jump and hit sounds and now enemies follows the player if nearby.

--- slime.gd
+++ slime.gd
@@ -15,3 +15,23 @@ func _process(delta: float) -> void:
 		$AnimatedSprite2D.flip_h = false
 
 	position.x += direction * SPEED * delta
+
+func set_direction_to_body(body: Node2D) -> void:
+	#sets direction to body position
+	if(position.x>body.position.x):
+		direction = -1
+		$AnimatedSprite2D.flip_h = true
+	else:
+		direction = 1
+		$AnimatedSprite2D.flip_h = false
+	
+func _on_detect_zone_body_entered(body: Node2D) -> void:
+	$StateChart.send_event("enemy_entered")
+	
+	set_direction_to_body(body)
+
+
+func _on_detect_zone_body_exited(body: Node2D) -> void:
+	$StateChart.send_event("enemy_out")
+	
+	set_direction_to_body(body)
